6d65b0629fe0d272696fbe40765ad2df1f793956
Jonas Natten <jonasn12@student.uia.no>
2015-11-12 03:51:16 +0100

    Fixes so gui uses properties from class instead of hardcoded

--- src/GUI.java
+++ src/GUI.java
@@ -78,27 +78,25 @@ public class GUI extends JFrame {
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        String uri = "http://jonasn12.uia.io/ontology#";
-
         // Create main ontology model
         model = new OntProp().getUpdatedModel();
 
         //Create classes
-        OntClass topics = model.createClass(uri + "Topics");
-        OntClass courses = model.createClass(uri + "Courses");
-        OntClass learningTypes = model.createClass(uri + "Learningtypes");
+        OntClass topics = model.createClass(OntologyProperties.URI + "Topics");
+        OntClass courses = model.createClass(OntologyProperties.URI + "Courses");
+        OntClass learningTypes = model.createClass(OntologyProperties.URI + "Learningtypes");
 
         //Create some learning types
-        Individual presentation = model.createIndividual(uri + "Presentation", learningTypes);
-        Individual lecture = model.createIndividual(uri + "Lecture", learningTypes);
+        Individual presentation = model.createIndividual(OntologyProperties.URI + "Presentation", learningTypes);
+        Individual lecture = model.createIndividual(OntologyProperties.URI + "Lecture", learningTypes);
 
         //Create some topics
-        Individual advancedSubject = model.createIndividual(uri + "Integrals", topics);
-        Individual baseSubject = model.createIndividual(uri + "Algebra", topics);
+        Individual advancedSubject = model.createIndividual(OntologyProperties.URI + "Integrals", topics);
+        Individual baseSubject = model.createIndividual(OntologyProperties.URI + "Algebra", topics);
 
         //Create some courses
-        Individual mathCourse = model.createIndividual(uri + "MA-154", courses);
-        Individual programmingCourse = model.createIndividual(uri + "DAT101", courses);
+        Individual mathCourse = model.createIndividual(OntologyProperties.URI + "MA-154", courses);
+        Individual programmingCourse = model.createIndividual(OntologyProperties.URI + "DAT101", courses);
 
         //Add some properties
         model.add(topics,OntProp.hasSubtopic,advancedSubject);
@@ -133,7 +131,7 @@ public class GUI extends JFrame {
             public void actionPerformed(ActionEvent e) {
                 String resName = newTopic.getText();
 
-                newResource = model.createIndividual(uri + resName, topics);
+                newResource = model.createIndividual(OntologyProperties.URI + resName, topics);
                 model.add(topics, OntProp.hasSubtopic, newResource);
 
                 if (!dependantOn.isEmpty()) {
@@ -154,12 +152,12 @@ public class GUI extends JFrame {
                     for (String item : types) {
 
                         if (item.equals("Presentation")) {
-                            newResource.addProperty(OntProp.hasPracticalPart, model.getIndividual(uri + item));
-                            model.getIndividual(uri + item).addProperty(OntProp.isPracticalPart, newResource);
+                            newResource.addProperty(OntProp.hasPracticalPart, model.getIndividual(OntologyProperties.URI + item));
+                            model.getIndividual(OntologyProperties.URI + item).addProperty(OntProp.isPracticalPart, newResource);
 
                         } else if (item.equals("Lecture")) {
-                            newResource.addProperty(OntProp.hasTheoreticalPart, model.getIndividual(uri + item));
-                            model.getIndividual(uri + item).addProperty(OntProp.isTheoreticalPart, newResource);
+                            newResource.addProperty(OntProp.hasTheoreticalPart, model.getIndividual(OntologyProperties.URI + item));
+                            model.getIndividual(OntologyProperties.URI + item).addProperty(OntProp.isTheoreticalPart, newResource);
                         }
                     }
                 }
@@ -176,7 +174,7 @@ public class GUI extends JFrame {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String cName = courseName.getText();
-                newResource = model.createIndividual(uri + cName, courses);
+                newResource = model.createIndividual(OntologyProperties.URI + cName, courses);
                 model.add(courses, OntProp.hasTopic, newResource);
 
                 if(!courseHasTopic.isEmpty()){
